8a89f716885955348131dacb1e7ed4c1bd4dce4c
Franperman23 <franperman2323@gmail.com>
2025-11-25 09:30:31 +0100

    Refran script.js

--- public/script.js
+++ public/script.js
@@ -8,11 +8,17 @@ setInterval(updateTime, 1000);
 updateTime();
 
 // Llamada simple a una API pública
-fetch("https://api.popcat.xyz/refranes")
+fetch("https://refranero-api.vercel.app/refran/random")
     .then(res => res.json())
     .then(data => {
         document.getElementById("api-result").textContent =
             "Refrán: " + data.refran;
+    })
+    .catch(err => {
+        document.getElementById("api-result").textContent =
+            "Error al cargar refrán";
+        console.error(err);
     });
+
 // Variable de entorno
 console.log("Variable desde Vercel:", process.env.VITE_SALUDO);
